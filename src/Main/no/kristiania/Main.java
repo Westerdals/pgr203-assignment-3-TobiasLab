6268159f3ba6e8e716aeb8aee787e341100ad7ca
TobiasLab <tobias.labidi@gmail.com>
2019-10-22 14:49:02 +0200

    Fixed up main file. Still some error.

--- src/Main/no/kristiania/Main.java
+++ src/Main/no/kristiania/Main.java
@@ -1,8 +1,64 @@
 package no.kristiania;
+import org.postgresql.ds.PGSimpleDataSource;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class Main {
-    
-    public static void main(String[] args) {
 
+    private final MemberDao memberDao;
+    private PGSimpleDataSource dataSource;
+    private BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    private ProjectDao projectDao;
+
+    public Main() throws IOException {
+        Properties properties = new Properties();
+        properties.load(new FileReader("main.properties"));
+
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+
+
+
+        projectDao = new ProjectDao(dataSource);
+        memberDao = new MemberDao(dataSource);
+    }
+
+    public static void main(String[] args) throws IOException, SQLException {
+        new Main().run();
+    }
+
+    private static void run() throws IOException, SQLException {
+        System.out.println("Choose action: create [project]|create [member]");
+        String action = input.readLine();
+
+        switch (action.toLowerCase()) {
+            case "project":
+                insertProject();
+                break;
+            case "member":
+                insertMember();
+                break;
+        }
+
+        System.out.println("Current projects " + projectDao.listAll());
+    }
+
+    private void insertMember() throws IOException, SQLException {
+        System.out.println("Please type the name of a new member");
+        Member member = new member();
+        member.setName(input.readLine());
+        memberDao.insert(member);
+    }
+
+    private void insertProject() throws IOException, SQLException {
+        System.out.println("Please type the name of a new project");
+        String projectName = input.readLine();
+        Project project = new Project();
+        project.setName(projectName);
+        projectDao.insert(project);
     }
 }
