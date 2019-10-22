4649250b5398a8492c5cd69c5b217142372c5978
TobiasLab <tobias.labidi@gmail.com>
2019-10-22 12:27:42 +0200

    Removed VCS tests.

--- src/Main/no/kristiania/Project.java
+++ src/Main/no/kristiania/Project.java
@@ -1,9 +1,5 @@
 package no.kristiania;
 
 public class Project {
-    public static String hello = "Hvad skjer mine menn";
 
-    public static void main(String[] args) {
-        System.out.println(hello);
-    }
 }
