20b1c578422c427fd12688135c0ba65353caf751
Edwin <edwin_engelbrethsen@outlook.com>
2019-10-22 13:04:02 +0200

    filled projectDao

--- src/Main/no/kristiania/MemberDao.java
+++ src/Main/no/kristiania/MemberDao.java
@@ -1,18 +1,21 @@
 package no.kristiania;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MemberDao {
+    private List<Member> members = new ArrayList<>();
+
     public MemberDao(DataSource dataSource) {
 
     }
 
     public void insert(Member member) {
-
+        members.add(member);
     }
 
     public List<Member> listAll() {
-        return null;
+        return members;
     }
 }
--- src/Main/no/kristiania/ProjectDao.java
+++ src/Main/no/kristiania/ProjectDao.java
@@ -1,5 +1,42 @@
 package no.kristiania;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProjectDao {
 
+    private DataSource dataSource;
+
+    public ProjectDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void insert(String project) throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "insert into projects (name) values (?)";
+            try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+                stmt.setString(1, project);
+                stmt.executeUpdate();
+            }
+        }
+    }
+
+    public List<String> listAll() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement stmt = connection.prepareStatement("select * from projects")) {
+                try (ResultSet rs = stmt.executeQuery()) {
+                    List<String> projects = new ArrayList<>();
+                    while (rs.next()) {
+                        projects.add(rs.getString("name"));
+                    }
+                    return projects;
+                }
+            }
+        }
+    }
 }
