4e81deb806994b66ad0ad3addfbbae058ff8c56a
TobiasLab <tobias.labidi@gmail.com>
2019-10-22 12:47:03 +0200

    Add first test to memberdaotest

--- src/test/no/kristiania/MemberDaoTest.java
+++ src/test/no/kristiania/MemberDaoTest.java
@@ -2,10 +2,18 @@ package no.kristiania;
 
 import org.junit.jupiter.api.Test;
 
+import javax.sql.DataSource;
+
 public class MemberDaoTest {
+
+    private DataSource dataSource;
 
     @Test
     void shouldListProjectMember() {
+        Member member = new Member();
+        MemberDao dao = new MemberDao(dataSource);
 
+        dao.insert(member);
+        assertThat(dao.listAll()).contains(order);
     }
 }
